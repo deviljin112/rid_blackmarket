a8ed9b6e69a3f26f5611016b6e6ee4fbecb6f36a
deviljin112 <39461646+deviljin112@users.noreply.github.com>
2020-04-20 14:19:48 +0100

    fxmanifest update

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Dev x BTNGaming'
+description 'ESX Weapon Black Market'
+version '1.0'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}
