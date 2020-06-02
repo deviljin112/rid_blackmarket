4b4e698a16bf1518997dc973607f6fcff817713b
Gabriel Calderon <835733+gabrielcalderon@users.noreply.github.com>
2020-06-02 04:26:35 -0400

    Update es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -1,13 +1,13 @@
 Locales ['es'] = {
-  ['buy_license'] = '¿Comprar licencia de arma?',
+  ['buy_license'] = '¿Comprar licencia de armas?',
   ['yes'] = '%s',
   ['no'] = 'no',
-  ['weapon_bought'] = 'comprado por %s EUR',
+  ['weapon_bought'] = 'comprado por %s',
   ['not_enough_black'] = 'usted no tiene ~r~suficiente~s~ dinero negro',
   ['not_enough'] = 'usted no tiene ~r~suficiente~s~ dinero',
   ['already_owned'] = ' ¡Ya tienes esta arma!',
   ['shop_menu_title'] = 'Tienda',
   ['shop_menu_prompt'] = 'Presione ~INPUT_CONTEXT~ para acceder a la tienda.',
-  ['shop_menu_item'] = '%s EUR',
+  ['shop_menu_item'] = '%s',
   ['map_blip'] = 'Mercado Negro',
 }
