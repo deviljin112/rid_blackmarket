e2f36156596c3685a9be6354318f8080bad4f8fd
Thuan Truong <21116370+NewT74@users.noreply.github.com>
2020-08-19 01:56:32 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,13 @@
 local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+	['ESC'] = 322, ['F1'] = 288, ['F2'] = 289, ['F3'] = 170, ['F5'] = 166, ['F6'] = 167, ['F7'] = 168, ['F8'] = 169, ['F9'] = 56, ['F10'] = 57,
+	['~'] = 243, ['1'] = 157, ['2'] = 158, ['3'] = 160, ['4'] = 164, ['5'] = 165, ['6'] = 159, ['7'] = 161, ['8'] = 162, ['9'] = 163, ['-'] = 84, ['='] = 83, ['BACKSPACE'] = 177,
+	['TAB'] = 37, ['Q'] = 44, ['W'] = 32, ['E'] = 38, ['R'] = 45, ['T'] = 245, ['Y'] = 246, ['U'] = 303, ['P'] = 199, ['['] = 39, [']'] = 40, ['ENTER'] = 18,
+	['CAPS'] = 137, ['A'] = 34, ['S'] = 8, ['D'] = 9, ['F'] = 23, ['G'] = 47, ['H'] = 74, ['K'] = 311, ['L'] = 182,
+	['LEFTSHIFT'] = 21, ['Z'] = 20, ['X'] = 73, ['C'] = 26, ['V'] = 0, ['B'] = 29, ['N'] = 249, ['M'] = 244, [','] = 82, ['.'] = 81,
+	['LEFTCTRL'] = 36, ['LEFTALT'] = 19, ['SPACE'] = 22, ['RIGHTCTRL'] = 70,
+	['HOME'] = 213, ['PAGEUP'] = 10, ['PAGEDOWN'] = 11, ['DELETE'] = 178,
+	['LEFT'] = 174, ['RIGHT'] = 175, ['TOP'] = 27, ['DOWN'] = 173,
+	['NENTER'] = 201, ['N4'] = 108, ['N5'] = 60, ['N6'] = 107, ['N+'] = 96, ['N-'] = 97, ['N7'] = 117, ['N8'] = 61, ['N9'] = 118
 }
 
 ESX = nil
@@ -24,19 +24,9 @@ Citizen.CreateThread(function()
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
-
-	--[[
-	ESX.TriggerServerCallback('rid_blackmarket:getShop', function(shopItems)
-		for k,v in pairs(shopItems) do
-			Config.Zones[k].Items = v
-		end
-	end)
-	]]--
-
 	while ESX.GetPlayerData().job == nil do
 		Citizen.Wait(10)
 	end
-
 	PlayerData = ESX.GetPlayerData()
 end)
 
@@ -50,31 +40,24 @@ end)
 function OpenShopMenu(zone)
 	local elements = {}
 	ShopOpen = true
-
-	Config.Zones["BlackMarket"].Items = nil
-
+	Config.Zones['BlackMarket'].Items = nil
 	-- Get Current Stock Logic
 	TriggerServerEvent('rid_blackmarket:stockUpdate')
-
 	-- Stop Menu opening before new stock data is loaded
-	while Config.Zones["BlackMarket"].Items == nil do
+	while Config.Zones['BlackMarket'].Items == nil do
 		Citizen.Wait(10)
 	end
-	
 	for i=1, #Config.Zones[zone].Items, 1 do
 		local item = Config.Zones[zone].Items[i]
-
 		table.insert(elements, {
-			label = ('%s - <span style="color: green;">%s</span> - <span style="color: red;">%s</span>'):format(item.label, _U('shop_menu_item', ESX.Math.GroupDigits(item.price)), item.stock),
+			label = ('%s - <span style='color: green;'>%s</span> - <span style='color: red;'>%s</span>'):format(item.label, _U('shop_menu_item', ESX.Math.GroupDigits(item.price)), item.stock),
 			price = item.price,
 			weaponName = item.item,
 			stock = item.stock
 		})
 	end
-
 	ESX.UI.Menu.CloseAll()
 	PlaySoundFrontend(-1, 'BACK', 'HUD_AMMO_SHOP_SOUNDSET', false)
-
 	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop', {
 		title = _U('shop_menu_title'),
 		align = 'top-left',
@@ -93,9 +76,8 @@ function OpenShopMenu(zone)
 		PlaySoundFrontend(-1, 'BACK', 'HUD_AMMO_SHOP_SOUNDSET', false)
 		ShopOpen = false
 		menu.close()
-
-		CurrentAction     = 'shop_menu'
-		CurrentActionMsg  = _U('shop_menu_prompt')
+		CurrentAction = 'shop_menu'
+		CurrentActionMsg = _U('shop_menu_prompt')
 		CurrentActionData = { zone = zone }
 	end, function(data, menu)
 		PlaySoundFrontend(-1, 'NAV', 'HUD_AMMO_SHOP_SOUNDSET', false)
@@ -105,24 +87,18 @@ end
 function DisplayBoughtScaleform(weaponName, price)
 	local scaleform = ESX.Scaleform.Utils.RequestScaleformMovie('MP_BIG_MESSAGE_FREEMODE')
 	local sec = 4
-
 	BeginScaleformMovieMethod(scaleform, 'SHOW_WEAPON_PURCHASED')
-
 	PushScaleformMovieMethodParameterString(_U('weapon_bought', ESX.Math.GroupDigits(price)))
 	PushScaleformMovieMethodParameterString(ESX.GetWeaponLabel(weaponName))
 	PushScaleformMovieMethodParameterInt(GetHashKey(weaponName))
 	PushScaleformMovieMethodParameterString('')
 	PushScaleformMovieMethodParameterInt(100)
-
 	EndScaleformMovieMethod()
-
 	PlaySoundFrontend(-1, 'WEAPON_PURCHASE', 'HUD_AMMO_SHOP_SOUNDSET', false)
-
 	Citizen.CreateThread(function()
 		while sec > 0 do
 			Citizen.Wait(0)
 			sec = sec - 0.01
-	
 			DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
 		end
 	end)
@@ -130,8 +106,8 @@ end
 
 AddEventHandler('rid_blackmarket:hasEnteredMarker', function(zone)
 	if zone == 'BlackMarket' then
-		CurrentAction     = 'shop_menu'
-		CurrentActionMsg  = _U('shop_menu_prompt')
+		CurrentAction = 'shop_menu'
+		CurrentActionMsg = _U('shop_menu_prompt')
 		CurrentActionData = { zone = zone }
 	end
 end)
@@ -149,111 +125,84 @@ AddEventHandler('onResourceStop', function(resource)
 	end
 end)
 
---[[ Create Blips
-Citizen.CreateThread(function()
-	for k,v in pairs(Config.Zones) do
-		if v.Legal then
-			for i = 1, #v.Locations, 1 do
-				local blip = AddBlipForCoord(v.Locations[i])
-
-				SetBlipSprite (blip, 110)
-				SetBlipDisplay(blip, 4)
-				SetBlipScale  (blip, 1.0)
-				SetBlipColour (blip, 81)
-				SetBlipAsShortRange(blip, true)
-
-				BeginTextCommandSetBlipName("STRING")
-				AddTextComponentSubstringPlayerName(_U('map_blip'))
-				EndTextCommandSetBlipName(blip)
-			end
-		end
-	end
-end)
-]]--
-
 -- Display markers
 Citizen.CreateThread(function()
-	Citizen.Wait(500)
-
-	for k,v in pairs(Config.Zones) do
-		for i = 1, #v.Locations, 1 do
-			location = v.Locations[i]
-		end
-	end
-
 	while true do
-		Citizen.Wait(0)
-		local coords = GetEntityCoords(PlayerPedId())
-
-		if PlayerData.job and  PlayerData.job.name ~= 'police' then
-			if (Config.Type ~= -1 and GetDistanceBetweenCoords(coords, location, true) < Config.DrawDistance) then
-				DrawMarker(Config.Type, location, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
-			end
-		elseif Config.Police_Visibility then 
-			if (Config.Type ~= -1 and GetDistanceBetweenCoords(coords, location, true) < Config.DrawDistance) then
-				DrawMarker(Config.Type, location, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
+		local wait = 500
+		for k, v in pairs(Config.Zones) do
+			for i = 1, #v.Locations, 1 do
+				local location = v.Locations[i]
+				local coords = GetEntityCoords(PlayerPedId())
+				local distance = #(coords - location)
+				if distance < 50.0 then
+					wait = 1
+					if PlayerData.job and  PlayerData.job.name ~= 'police' then
+						if (Config.Type ~= -1 and distance < Config.DrawDistance) then
+							DrawMarker(Config.Type, location, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
+						end
+					elseif Config.Police_Visibility then 
+						if (Config.Type ~= -1 and distance < Config.DrawDistance) then
+							DrawMarker(Config.Type, location, 0.0, 0.0, 0.0, 0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, false, false, false)
+						end
+					end
+					break
+				end
 			end
 		end
+		Citizen.Wait(wait)
 	end
 end)
 
 -- Enter / Exit marker events
 Citizen.CreateThread(function()
-	Citizen.Wait(500)
-	PlayerData = ESX.GetPlayerData()
-
 	while true do
-		Citizen.Wait(0)
-
+		local wait = 500
 		local coords = GetEntityCoords(PlayerPedId())
 		local isInMarker, currentZone = false, nil
-
-		for k,v in pairs(Config.Zones) do
+		for k, v in pairs(Config.Zones) do
 			for i = 1, #v.Locations, 1 do
-				if GetDistanceBetweenCoords(coords, v.Locations[i], true) < Config.Size.x then
-					isInMarker, ShopItems, currentZone, LastZone = true, v.Items, k, k
+				local distance = #(coords - v.Locations[i])
+				if distance < 50.0 then
+					wait = 1
+					if distance < Config.Size.x then
+						isInMarker, ShopItems, currentZone, LastZone = true, v.Items, k, k
+					end
+					break
 				end
 			end
 		end
-
-		if PlayerData.job.name ~= 'police' then
+		if PlayerData.job and PlayerData.job.name ~= 'police' then
 			if isInMarker and not HasAlreadyEnteredMarker then
 				HasAlreadyEnteredMarker = true
 				TriggerEvent('rid_blackmarket:hasEnteredMarker', currentZone)
 			end
 		end
-
 		if isInMarker and not HasAlreadyEnteredMarker and Config.Police_Use then
 			HasAlreadyEnteredMarker = true
 			TriggerEvent('rid_blackmarket:hasEnteredMarker', currentZone)
 		end
-		
-		
 		if not isInMarker and HasAlreadyEnteredMarker then
 			HasAlreadyEnteredMarker = false
 			TriggerEvent('rid_blackmarket:hasExitedMarker', LastZone)
 		end
+		Citizen.Wait(wait)
 	end
 end)
 
 -- Key Controls
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-
-		if CurrentAction ~= nil then
-			ESX.ShowHelpNotification(CurrentActionMsg)
-
-			if IsControlJustReleased(0, Keys['E']) then
-
+    while true do
+        Citizen.Wait(1)
+        if CurrentAction ~= nil then
+            ESX.ShowHelpNotification(CurrentActionMsg)
+            if IsControlJustReleased(0, Keys['E']) then
                 if CurrentAction == 'shop_menu' then
-                    
                     OpenShopMenu(CurrentActionData.zone)
-                    
-				end
-
-				CurrentAction = nil
-			end
-		end
-	end
+                end
+                CurrentAction = nil
+            end
+        else
+        	Citizen.Wait(500)
+        end
+    end
 end)
